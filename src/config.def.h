986193805ebd580da0e49a82357e26e0d63a2c43
shawarmateam <adisteyfgmd@gmail.com>
2024-11-25 13:00:57 +0300

    changed file

---
+++ src/config.def.h
@@ -0,0 +1,28 @@
+#ifndef CONFIG_H
+#define CONFIG
+
+#define BLUE "\033[34m"
+#define YELLOW "\033[1;33m"
+#define GREEN "\033[1;32m"
+#define CYAN "\033[0;36m"
+#define WHITE "\033[1;37m"
+#define PINK "\033[1;35m"
+
+#define NAME CYAN
+#define AT   WHITE
+#define HOST PINK
+
+#define ICON0 BLUE
+#define ICON1 BLUE
+#define ICON2 BLUE
+#define ICON3 BLUE
+#define ICON4 BLUE
+#define ICON5 BLUE
+#define ICON6 BLUE
+#define ICON7 BLUE
+
+#define EMOJI YELLOW
+#define ARROW YELLOW
+#define VAL   GREEN
+
+#endif // CONFIG_H
